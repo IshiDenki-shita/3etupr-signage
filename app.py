13994a66a481cedeb8a42a1b54eccdc9d7bbffa5
yuto-ETUPR <defects-sells8o@icloud.com>
2026-01-14 18:18:14 +0900

    change json path

--- app.py
+++ app.py
@@ -82,10 +82,7 @@ def page1():
 # 担当：中田と齋藤
 @app.route("/page2")
 def page2():
-    # commit_test
-
     # --- app.py のロジックをここに統合 ---
-    # 必要なモジュールをこの関数内でのみインポート（他への影響なし）
     from datetime import datetime, timedelta
 
     OPERATIONAL_START_HOUR = 4
